18848905309672633c4aee6e2274b97b83084c29
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 14:49:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library(file)
   library = {get_meaning => {}, get_emoticon => {}}
     
   path.each do |key, value|
-    get_meaning << key[value(0)]
-    get_emoticon << key[value(1)]
+    library[get_meaning] << key[value(0)]
+    library[get_emoticon] << key[value(1)]
   end
   return library
 end
