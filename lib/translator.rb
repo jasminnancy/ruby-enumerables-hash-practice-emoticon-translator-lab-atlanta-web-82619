e52cb570f8a76c5e84a576bd0e45df991e4ffc3c
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 15:03:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,18 @@ require "yaml"
 
 def load_library(file)
   path = YAML.load_file(file)
+    
+  path.each do |key, value|
+    if path[:get_meaning] && path[:get_emoticon]
+      path[:get_meaning] = key[value(0)]
+      path[:get_emoticon] = key[value(1)]
+    else
+      path[:get_meaning] = {}
+      path[:get_meaning] = key[value(0)]
+      path[:get_emoticon] = {}
+      path[:get_emoticon] = key[value(1)]
+    end
+  end
 end
 
 def get_japanese_emoticon(path, emote)
