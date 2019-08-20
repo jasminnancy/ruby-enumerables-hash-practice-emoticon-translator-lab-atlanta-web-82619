26daf690fa6e6868bd2bf9514022ec3852748bb9
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 14:47:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,13 @@ require "yaml"
 
 def load_library(file)
   path = YAML.load_file(file)
-  library = {}
+  library = {get_meaning => {}, get_emoticon => {}}
     
   path.each do |key, value|
-    
+    get_meaning << key[value(0)]
+    get_emoticon << key[value(1)]
   end
+  return library
 end
 
 def get_japanese_emoticon(path, emote)
