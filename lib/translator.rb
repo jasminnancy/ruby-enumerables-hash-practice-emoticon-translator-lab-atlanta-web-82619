284af737c9f51c40be8b7841af7f3d25c53aa8a9
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-20 15:32:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file)
   path.each do |meaning, emote_array|
     #binding.pry
     library[:get_meaning][emote_array[1]] = meaning
-    library[:get_emoticon][emote_array[0]] = [emote_array[1]]
+    library[:get_emoticon][emote_array[0]] = emote_array[1]
   end
   return library
 end
